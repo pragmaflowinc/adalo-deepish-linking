875889ed039c847cf6d0a39831d7f685987832d0
Steven McKeown <steven@pragmaflow.com>
2021-11-27 11:29:05 -0500

    should work on iOS now

--- scripts/install_ios.sh
+++ scripts/install_ios.sh
@@ -2,6 +2,26 @@
 set -e
 set -x
 
-npx uri-scheme add yourChoosenScheme --ios
+dir=$(dirname "${0}")
+uriScheme=$("${dir}/get_uri_data.js" uriScheme) 
+urlHostname=$("${dir}/get_uri_data.js" urlHostname) 
+
+name=$PROJECT_NAME
+
+plutil -insert CFBundleURLTypes -xml "
+<array>
+  <dict>
+    <key>CFBundleURLSchemes</key>
+    <array>
+      <string>$uriScheme</string>
+    </array>
+  </dict>
+  <dict>
+    <key>CFBundleURLSchemes</key>
+    <array>
+      <string>$uriHostname</string>
+    </array>
+  </dict>
+</array>" ios/$name/Info.plist
 
 echo "configured iOS"
